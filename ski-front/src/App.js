112eba3a93808d952176d4a3528a95a50f13558a
JACOB HANSEN <hansen6239@uwlax.edu>
2026-03-02 20:23:04 -0600

    Init commit of brand new front end

---
+++ ski-front/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ButtonAppBar from './comps/baseAppBar';
+
+function App() {
+
+  return (
+    <div className="App">
+      <ButtonAppBar></ButtonAppBar>
+      
+    </div>
+  );
+}
+
+export default App;

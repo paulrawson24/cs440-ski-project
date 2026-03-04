55dbf35595560ac84e5b7cea5d195978d73cd3b6
ewateski2 <wateski3978@uwlax.edu>
2026-03-04 17:14:09 -0600

    changes

--- ski-back/server.js
+++ ski-back/server.js
@@ -9,11 +9,13 @@ const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin");
 const coachRoutes = require("./routes/coach");
 const skierRoutes = require("./routes/skier");
+const registerRoute = require("./routes/register");
 
 console.log("authRoutes type:", typeof authRoutes);
 console.log("adminRoutes type:", typeof adminRoutes);
 console.log("coachRoutes type:", typeof coachRoutes);
 console.log("skierRoutes type:", typeof skierRoutes);
+console.log("registerRoutes type:", typeof registerRoute);
 
 const app = express();
 app.use(cors());
@@ -25,6 +27,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/coach", coachRoutes);
 app.use("/api/skier", skierRoutes);
+app.use("/api/register", registerRoute);
 
 const PORT = 4000;
 app.listen(PORT, () => console.log(`API running on ${PORT}`));
